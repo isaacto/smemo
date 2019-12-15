4688da125815524c1813c31d52b4671cc4d0b426
Isaac To <isaacto@gmail.com>
2019-12-15 22:33:13 +0800

    Set version number to 0.2
    Signed-off-by: Isaac To <isaacto@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='smemo',
-    version='0.1.1',
+    version='0.2',
     python_requires='~=3.5',
     author='Isaac To',
     author_email='isaac.to@gmail.com',
